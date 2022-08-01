8b7d728d3a38b266371b46564eed138e2b288011
riky <pembuatan api selesai>
2022-08-02 02:16:31 +0700

    new

---
+++ resources/views/book/read.blade.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css" integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous">
+    <title>Document</title>
+</head>
+<body>
+     <!-- <iframe src = {{ url('filebook/', $book->file) }} width='100%' height='724' allowfullscreen webkitallowfullscreen></iframe> -->
+
+     <!--<embed src="https://online.fliphtml5.com/amzct/qyuu/#p=1" type="application/pdf" width="100%"-->
+     <!-- height="724"></embed>-->
+     <iframe src="https://fliphtml5.com/bookcase/aytwb" width="100%" height="637" frameborder="0" webkitAllowFullScreen mozallowfullscreen allowFullScreen></iframe>
+</body>
+<!-- <body>
+     <iframe src = "{{ url('filebook/', $book->file) }}" width='100%' height='724' allowfullscreen webkitallowfullscreen></iframe>
+</body> -->
+</html>
